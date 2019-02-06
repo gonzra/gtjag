287265b176c6e46a62f85eed73f817ada3de07c9
Rafael E. Gonzalez <regonzalez@gmail.com>
2019-02-05 23:54:08 -0500

    Initial commit

--- leetcode/src/main/java/com/gonzra/gtjag/TwoSum.java
+++ leetcode/src/main/java/com/gonzra/gtjag/TwoSum.java
@@ -1,4 +1,51 @@
 package com.gonzra.gtjag;
 
+/*
+ * Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+ *
+ * You may assume that each input would have exactly one solution, and you may not use the same element twice.
+ *
+ * Example:
+ *
+ * Given nums = [2, 7, 11, 15], target = 9,
+ *
+ * Because nums[0] + nums[1] = 2 + 7 = 9,
+ * return [0, 1].
+ */
+
+import org.omg.PortableInterceptor.INACTIVE;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class TwoSum {
+
+    /*
+    Questions and Assumptions
+    - Could nums be empty or null?
+    - Could target be negative?
+    Lets assume that nums can be null or empty. Also, lets assume that target can be negative or positive
+     */
+
+    public int[] twoSum(int[] nums, int target) {
+        if (nums == null || nums.length == 0) {
+            throw new IllegalArgumentException("Input array \"nums\" cannot be neither null nor empty");
+        }
+
+        int[] result = new int[2];
+        Map<Integer, Integer> indexMap = new HashMap<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            int currValue = nums[i];
+
+            if (indexMap.containsKey(target - currValue)) {
+                result[0] = indexMap.get(target - currValue);
+                result[1] = i;
+                return result;
+            }
+            indexMap.put(currValue, i);
+        }
+        return result;
+    }
+
 }
--- leetcode/src/test/java/com/gonzra/gtjag/TwoSumTest.java
+++ leetcode/src/test/java/com/gonzra/gtjag/TwoSumTest.java
@@ -1,4 +1,54 @@
 package com.gonzra.gtjag;
 
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+
+import static org.hamcrest.CoreMatchers.is;
+
 public class TwoSumTest {
+
+    @Rule
+    public ExpectedException exceptionRule = ExpectedException.none();
+
+    @Test
+    public void nullInput() {
+        exceptionRule.expect(IllegalArgumentException.class);
+        int[] nums = null;
+        int target = 13;
+        int[] result = test(nums, target);
+    }
+
+    @Test
+    public void emptyInput() {
+        exceptionRule.expect(IllegalArgumentException.class);
+        int[] nums = {};
+        int target = 13;
+        int[] result = test(nums, target);
+    }
+
+    @Test
+    public void regularInput() {
+        int[] nums = {2, 7, 11, 15};
+        int target = 9;
+        int[] result = test(nums, target);
+        Assert.assertThat(result.length, is(2));
+        Assert.assertThat(result, is(new int[]{0,1}));
+    }
+
+    @Test
+    public void negativeTarget() {
+        int[] nums = {-1, 2, 7, 11, 15, -3};
+        int target = -4;
+        int[] result = test(nums, target);
+        Assert.assertThat(result.length, is(2));
+        Assert.assertThat(result, is(new int[]{0,5}));
+    }
+
+    private int[] test(int[] nums, int target) {
+        TwoSum twoSumInstance = new TwoSum();
+        return twoSumInstance.twoSum(nums, target);
+    }
 }
